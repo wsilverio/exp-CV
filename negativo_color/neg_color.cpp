a46f67a95462a98a4973263025b064b0b3a932fb
wsilverio <wendeurick@gmail.com>
2014-12-05 16:54:50 -0200

    arquivos atualizados

--- negativo_color/neg_color.cpp
+++ negativo_color/neg_color.cpp
@@ -6,15 +6,15 @@ using namespace cv;
 // using namespace std;
 
 int main(void){
-    Mat img = imread("../images/Lenna.png");
+	Mat img = imread("../images/Lenna.png");
     // resize(img, img, Size(), 0.15, 0.15);
-    imshow("original", img);
-    imwrite("original.png", img);
+	imshow("original", img);
+	imwrite("original.png", img);
 
-    imshow("negativo", Scalar(255, 255, 255) - img);
-    imwrite("negativo.png", Scalar(255, 255, 255) - img);
+	imshow("negativo", Scalar(255, 255, 255) - img);
+	imwrite("negativo.png", Scalar(255, 255, 255) - img);
 
-    waitKey();
+	waitKey();
 
-    return 0;
+	return 0;
 }
